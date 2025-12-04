3eeecbb7832c124aa47de6bcb6019c6682e29a94
Aqilbek230 <seyilbekovaqilbek91@gmail.com>
2025-12-04 23:24:30 +0500

    for sikli janalandi

--- for_skil.py
+++ for_skil.py
@@ -47,7 +47,6 @@
 # print(sanlar_kvadrati)   
 
 # doslar=[]
-# print("5 dostinizidn atin kiritin")
 # for n in range(5):
 #     doslar.append(input(f"{n+1} dostinnin atin kiritin:"))
 # print(doslar)
