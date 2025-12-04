7b39dbf288659fd4480c475edd5cf03d7a46ad29
Aqilbek230 <seyilbekovaqilbek91@gmail.com>
2025-12-04 23:20:34 +0500

    for sikili boyinsha esaplar qoyildi

---
+++ for_skil.py
@@ -0,0 +1,163 @@
+"""
+       SEYLBEKOV AQILBEK
+                   PYTHON kurs
+           for-cikli                  13.11.2025
+"""
+#1
+# sanlar=[1,2,3,4,5]
+# for i in sanlar:
+#     print(i)
+    
+
+# for i in range(1,11):
+#     print(i)
+
+
+#2
+# miymanlar=["Aqilbek","Shamshet","Aman","Axmet"]
+# for i in miymanlar:
+#     print("Qalay", i)
+    
+#     print("dabay", i)
+    
+# print("Vsem dabay")
+
+
+
+#3
+# for i in range(1,11):
+#     print(i**2)
+
+#4
+# for i in range(1,11):
+#     print(f"1*{i}={i*1}")
+    
+
+#5
+# sanlar=list(range(1,11))
+# for san in sanlar:
+#     print(f"{san} kvadrati= {san**2}ge ten")
+    
+    
+# sanlar=list(range(1,11))   
+# sanlar_kvadrati=[]
+# for san in sanlar:
+#     sanlar_kvadrati.append(san**2)
+# print(sanlar)
+# print(sanlar_kvadrati)   
+
+# doslar=[]
+# print("5 dostinizidn atin kiritin")
+# for n in range(5):
+#     doslar.append(input(f"{n+1} dostinnin atin kiritin:"))
+# print(doslar)
+    
+    
+#ESAPLAR
+
+#1
+# k=int(input("k: "))
+# n=int(input("n: "))
+# for i in range(n):
+#     print(k)
+
+#2
+# a=int(input("a: "))
+# b=int(input("b: "))
+# s=0
+# for i in range(a,b+1):
+#     print(i)
+#     s+=1
+# print(s)
+
+#3
+# sanlar=[1,2,3,4]
+# s=0
+# sanlar.sort(reverse=True)
+# for i in sanlar:
+#     print(i)
+#     s+=1
+# print(s)
+
+#4
+# a=int(input("a: "))
+# for i in range(1,11):
+#     print(i*a)
+
+#5
+#a=int(input("a: "))
+# for i in range(1/10,11/10):
+#     print(i)
+
+
+#7
+# a=6
+# b=10
+# s=0
+# for i in range(a,b+1):
+#     s+=i
+# print(s)
+
+#8
+# a=2
+# b=4
+# s=1
+# for i in range(a,b+1):
+#     s*=i
+# print(s)
+
+
+#9
+# a=4
+# b=6
+# s=0
+# for i in range(a,b+1):
+#     s+=i**2
+# print(s)
+    
+
+#10
+# n=4
+# s=0
+# for i in range(1,n+1):
+#     s+=1/i
+# print(s)
+
+
+   
+# adamlar=["Aqilbek","Janibek","Arziwbay","Baxram","Rashid"]
+# for i in adamlar:
+#     print("Salem",i)
+    
+# print("kod",len(adamlar),"marte qaytalandi")
+
+
+# for i in range(11,100,2):
+#     print(i**3)
+
+# kinolar=[]
+# print("Jaqsi korgen 5 kinoniz: ")
+# for i in range(5):
+#     kinolar.append(input(f"{i+1} kinonizdin atin kiritin:"))
+# print(kinolar)
+
+
+# n = int(input("Bugun necha odam bilan uchrashdingiz: "))
+# adamalar = []
+# for i in range(n):
+#     adam = input(f"{i+1}-uchrashgan odamingizni kiriting: ")
+#     adamalar.append(adam)
+
+# print(adamalar)
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
