d15fcf1b9e3875efc774c18e9d637e71ff4de315
Akilbek.tech <seyilbekovaqilbek91@gmail.com>
2026-01-24 00:01:55 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ root.geometry("260x380")
 root.resizable(width=False,height=False)
 root['bg']="snow4"
 
-def bos(x):
+def bas(x):
     ekran.insert(END, str(x))
 
 def tozalash():
@@ -21,26 +21,26 @@ def hisobla():
         ekran.delete(0, END)
         ekran.insert(END, "Xato")
 
-Button(root,text=0,width=6,height=2,bg='white',command=lambda:bos(0)).place(x=5,y=335)
-Button(root,text=".",width=6,height=2,bg='white',command=lambda:bos(".")).place(x=60,y=335)
+Button(root,text=0,width=6,height=2,bg='white',command=lambda:bas(0)).place(x=5,y=335)
+Button(root,text=".",width=6,height=2,bg='white',command=lambda:bas(".")).place(x=60,y=335)
 Button(root,text="=",width=6,height=2,bg='white',command=hisobla).place(x=115,y=335)
 
-Button(root,text=1,width=6,height=2,bg='white',command=lambda:bos(1)).place(x=5,y=290)
-Button(root,text=2,width=6,height=2,bg='white',command=lambda:bos(2)).place(x=60,y=290)
-Button(root,text=3,width=6,height=2,bg='white',command=lambda:bos(3)).place(x=115,y=290)
+Button(root,text=1,width=6,height=2,bg='white',command=lambda:bas(1)).place(x=5,y=290)
+Button(root,text=2,width=6,height=2,bg='white',command=lambda:bas(2)).place(x=60,y=290)
+Button(root,text=3,width=6,height=2,bg='white',command=lambda:bas(3)).place(x=115,y=290)
 
-Button(root,text=4,width=6,height=2,bg='white',command=lambda:bos(4)).place(x=5,y=245)
-Button(root,text=5,width=6,height=2,bg='white',command=lambda:bos(5)).place(x=60,y=245)
-Button(root,text=6,width=6,height=2,bg='white',command=lambda:bos(6)).place(x=115,y=245)
+Button(root,text=4,width=6,height=2,bg='white',command=lambda:bas(4)).place(x=5,y=245)
+Button(root,text=5,width=6,height=2,bg='white',command=lambda:bas(5)).place(x=60,y=245)
+Button(root,text=6,width=6,height=2,bg='white',command=lambda:bas(6)).place(x=115,y=245)
 
-Button(root,text=7,width=6,height=2,bg='white',command=lambda:bos(7)).place(x=5,y=200)
-Button(root,text=8,width=6,height=2,bg='white',command=lambda:bos(8)).place(x=60,y=200)
-Button(root,text=9,width=6,height=2,bg='white',command=lambda:bos(9)).place(x=115,y=200)
+Button(root,text=7,width=6,height=2,bg='white',command=lambda:bas(7)).place(x=5,y=200)
+Button(root,text=8,width=6,height=2,bg='white',command=lambda:bas(8)).place(x=60,y=200)
+Button(root,text=9,width=6,height=2,bg='white',command=lambda:bas(9)).place(x=115,y=200)
 
-Button(root,text="+",width=6,height=5,bg='cyan',command=lambda:bos("+")).place(x=170,y=290)
-Button(root,text="-",width=6,height=2,bg='cyan',command=lambda:bos("-")).place(x=170,y=245)
-Button(root,text="*",width=6,height=2,bg='cyan',command=lambda:bos("*")).place(x=170,y=200)
-Button(root,text="/",width=6,height=2,bg='cyan',command=lambda:bos("/")).place(x=170,y=155)
+Button(root,text="+",width=6,height=5,bg='cyan',command=lambda:bas("+")).place(x=170,y=290)
+Button(root,text="-",width=6,height=2,bg='cyan',command=lambda:bas("-")).place(x=170,y=245)
+Button(root,text="*",width=6,height=2,bg='cyan',command=lambda:bas("*")).place(x=170,y=200)
+Button(root,text="/",width=6,height=2,bg='cyan',command=lambda:bas("/")).place(x=170,y=155)
 
 Button(root,text="C",width=6,height=2,bg='cyan',fg="red",command=tozalash).place(x=115,y=155)
 Button(root,text="%",width=6,height=2,bg='cyan',command=lambda:bos("%")).place(x=60,y=155)
